c6cc8dfb2721fe8758cab976ba99d58bf29bd283
Andrew Christie <awchristie86@gmail.com>
2021-04-11 18:22:44 -0400

    first commit'

---
+++ components/Todo.js
@@ -0,0 +1,13 @@
+/**
+ * @class       : Todo
+ * @author      : awc (awc@$HOSTNAME)
+ * @created     : Sunday Apr 11, 2021 13:52:16 EDT
+ * @description : Todo
+ */
+
+const Todo = () => {
+	return ( <h1>Hello!</h1> )
+}
+
+export default Todo;
+
